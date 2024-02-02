3bccadf67837288ad876753dbdcc5c7adb071c63
a.voloshko <cpz2xpn6gnx.dem1AWD>
2024-02-02 18:02:33 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Telegram bot for storing music that can be downloaded from YouTube. 
 The basic idea is that you download from YouTube link using a library [pytube](https://pytube.io/en/latest/) audio file and then send the downloaded file to the bot.
-This bot use library [pyTelegramBotAPI](https://pytba.readthedocs.io/en/latest/index.html).
+This bot use library [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot?tab=readme-ov-file).
--- main.py
+++ main.py
@@ -1,36 +1,25 @@
+import logging
 import os
 
-import telebot
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 
-from file_generator import FileGenerator
-from message import Message
-
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 load_dotenv()
 
-bot = telebot.TeleBot(os.getenv('BOT_API_KEY'))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    user = message.from_user
-    bot.send_message(message.chat.id, text=Message.start(user.first_name, user.last_name))
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv('BOT_API_KEY')).build()
 
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
 
-@bot.message_handler(commands=['help'])
-def help_command(message):
-    bot.send_message(message.chat.id, text=Message.help())
-
-
-@bot.message_handler(content_types=['text'])
-def processing_link_command(message):
-    audio_link = message.text
-    chat_id = message.chat.id
-    if not audio_link.startswith('https://'):
-        bot.send_chat_action(chat_id, 'typing')
-        bot.send_message(message.chat.id, text=Message.inappropriate_text())
-    else:
-        FileGenerator.generate_and_send_file(chat_id=chat_id, audio_link=audio_link, link_message_id=message.message_id)
-
-
-bot.polling(none_stop=True)
+    application.run_polling()
