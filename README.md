10dde4471924b020777cd9e39f590e11cd06158c
a.voloshko <cpz2xpn6gnx.dem1AWD>
2024-02-02 16:19:33 +0200

    edited README text. added additional message methods

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # telegrammusicbot
-A Telegram bot for storing music that can be downloaded from youtube
+
+A Telegram bot for storing music that can be downloaded from YouTube. 
+The basic idea is that you download from YouTube link using a library [pytube](https://pytube.io/en/latest/) audio file and then send the downloaded file to the bot.
+This bot use library [pyTelegramBotAPI](https://pytba.readthedocs.io/en/latest/index.html).
--- file_generator.py
+++ file_generator.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 
 import pytube.exceptions as exceptions
+import requests
 import telebot
 from dotenv import load_dotenv
 from pytube import YouTube, Stream
@@ -51,24 +52,28 @@ class FileGenerator:
             cls.bot.send_chat_action(chat_id, 'record_video')
             stream_obj = YouTube(audio_link)
             availability = stream_obj.check_availability()
-            if availability:
+            if not availability:
+                file_obj = stream_obj.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').first()
+                if file_obj.filesize < 20000000:
+                    file_path = cls.get_or_create_file(file_obj)
+                    downloaded_message_obj_id = cls.send_audio_file(
+                        file_path=file_path, chat_id=chat_id, downloading_message_obj_id=downloading_message_obj_id
+                    )
+                    cls.delete_unnecessary_file_and_messages(
+                        file_path=file_path, chat_id=chat_id, message_ids=[link_message_id, downloaded_message_obj_id]
+                    )
+                else:
+                    cls.bot.send_message(chat_id, text=Message.too_large_file())
+            else:
                 cls.bot.send_message(chat_id, text=Message.unavailable_link(availability))
         except (exceptions.VideoUnavailable, exceptions.RegexMatchError):
             cls.bot.send_message(chat_id, text=Message.unworked_link())
-        file_obj = stream_obj.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').first()
-        file_path = cls.get_or_create_file(file_obj)
-        downloaded_message_obj_id = cls.send_audio_file(
-            file_path=file_path, chat_id=chat_id, downloading_message_obj_id=downloading_message_obj_id
-        )
-        cls.delete_unnecessary_file_and_messages(
-            file_path=file_path, chat_id=chat_id, message_ids=[link_message_id, downloaded_message_obj_id]
-        )
 
     @classmethod
     def get_or_create_file(cls, file_obj: Stream) -> str:
         file_name = safe_filename(file_obj.title)
         if not os.path.isfile(os.path.dirname(os.path.abspath(__file__)) + '/files/' + file_name):
-            file_path = file_obj.download(filename=file_name)
+            file_path = file_obj.download(output_path='files', filename=file_name)
         else:
             file_path = str(os.path.dirname(os.path.abspath(__file__)) + '/files/' + file_name)
         return file_path
@@ -82,5 +87,18 @@ class FileGenerator:
             parse_mode='HTML',
         )
         with open(file_path, 'rb') as audio_file:
-            cls.bot.send_audio(chat_id, audio=audio_file)
-        return downloaded_message.message_id
+            cls.bot.send_chat_action(chat_id, 'record_video')
+            try:
+                cls.bot.send_audio(chat_id, audio=audio_file, timeout=300)
+                finished_message = cls.bot.edit_message_text(
+                    chat_id=chat_id,
+                    message_id=downloaded_message.message_id,
+                    text=Message.success_sent(),
+                )
+            except requests.exceptions.SSLError:
+                finished_message = cls.bot.edit_message_text(
+                    chat_id=chat_id,
+                    message_id=downloaded_message.message_id,
+                    text=Message.sending_audio_error(),
+                )
+        return finished_message.message_id
