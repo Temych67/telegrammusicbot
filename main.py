188032eca74ffc16ab9760335d230f21b3aa998b
a.voloshko <cpz2xpn6gnx.dem1AWD>
2024-02-05 13:04:22 +0200

    added additional command method

--- main.py
+++ main.py
@@ -12,14 +12,19 @@ logging.basicConfig(
 load_dotenv()
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Help you!")
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(os.getenv('BOT_API_KEY')).build()
 
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('start', start_command)
+    help_handler = CommandHandler('help', help_command)
     application.add_handler(start_handler)
+    application.add_handler(help_handler)
 
     application.run_polling()
